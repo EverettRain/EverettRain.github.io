68210bad9b267d48fd5ada1a7fc639675e83062e
EverettRain <13707331836@163.com>
2024-12-20 15:19:23 +0800

    Site updated: 2024-12-20 15:19:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/19/WheelCollider2/","2024/12/09/hello-world/","2024/12/16/prologue/","2024/12/11/搭建Hexo博客网站教程/","2024/12/11/更高级的Markdown渲染器/"];function toRandomPost(){
+var posts=["2024/12/09/hello-world/","2024/12/20/CylinderCaster/","2024/12/19/WheelCollider2/","2024/12/16/prologue/","2024/12/11/搭建Hexo博客网站教程/","2024/12/11/更高级的Markdown渲染器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
