73ef01781dc223de9eb2187c3c81bc80e17cfea6
Extra.#Flame <13707331836@163.com>
2025-02-01 21:19:01 +0800

    Site updated: 2025-02-01 21:19:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/26/C01P01/","2025/01/04/C01P02/","2025/01/31/C01P03/","2025/01/18/Chapter5/","2024/12/20/CylinderCaster/","2024/12/28/IntroductionReview/","2024/12/09/hello-world/","2024/12/16/prologue/","2024/12/19/WheelCollider2/","2024/12/11/搭建Hexo博客网站教程/","2024/12/11/更高级的Markdown渲染器/"];function toRandomPost(){
+var posts=["2024/12/26/C01P01/","2025/01/04/C01P02/","2025/01/31/C01P03/","2025/02/01/C01P04/","2025/01/18/Chapter5/","2024/12/28/IntroductionReview/","2024/12/20/CylinderCaster/","2024/12/09/hello-world/","2024/12/19/WheelCollider2/","2024/12/16/prologue/","2024/12/11/更高级的Markdown渲染器/","2024/12/11/搭建Hexo博客网站教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
